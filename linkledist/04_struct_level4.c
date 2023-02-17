8d977bf6c4ab94c64607bd7a55175e92accf6ca6
prokittikun <pro.kittikun.2546@gmail.com>
2023-02-17 13:28:47 +0700

    Struct 4 Levels:edit level 4

--- linkledist/04_struct_level4.c
+++ linkledist/04_struct_level4.c
@@ -1,18 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-// linkedlist
-struct Node__ {
+// linked list
+struct Node__
+{
     int value;
-    struct Node__* next;
+    struct Node__ *next;
 };
 
 typedef struct Node__ Node;
-typedef struct Node* NodePtr;
+typedef Node *NodePtr;
 
-void printList(NodePtr start){
-    
+void printList(NodePtr start)
+{
+    NodePtr current = start;
+    while (current != NULL)
+    {
+        printf("%d -> ", current->value);
+        current = current->next;
+    }
 }
-int main(){
-    NodePtr starPtr; // point to the start of linkedlist
-    starPtr = NULL;
+void addFirst(NodePtr *startPtr, int value)
+{
+    NodePtr temp = (NodePtr)malloc(sizeof(Node));
+    temp->value = value;
+    if (*startPtr == NULL)
+    {
+        temp->next = NULL; // temp will be the last Node
+        *startPtr = temp;
+    }
+    else
+    {
+        temp->next = *startPtr;
+        *startPtr = temp;
+    }
+}
+void addLast(NodePtr *startPtr, int value)
+{
+    NodePtr temp = (NodePtr)malloc(sizeof(Node));
+    temp->value = value;
+    temp->next = NULL;
+    if (*startPtr == NULL)
+    {
+        *startPtr = temp;
+    }
+    else
+    {
+        NodePtr current = *startPtr;
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = temp;
+    }
+}
+int main()
+{
+    NodePtr startPtr; // always point to the start of linked list
+    startPtr = NULL;  // init linked list
+    addLast(&startPtr, 0);
+    addFirst(&startPtr, 50);
+    addFirst(&startPtr, 90);
+    addFirst(&startPtr, 40);
+    addLast(&startPtr, 12);
+    addLast(&startPtr, 24);
+    addLast(&startPtr, 66);
+    printList(startPtr);
+
 }
