d111904522fadd2f96f70cf4008c5fc9fe886f56
prokittikun <pro.kittikun.2546@gmail.com>
2023-02-24 13:59:53 +0700

    Update: lab03:03

--- lab09.5/03_find_number.cpp
+++ lab09.5/03_find_number.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    int scan[n+1];
+    int max = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> scan[i];
+        if(max == 0 || max < scan[i]){
+            max = scan[i];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if(i != scan[i]){
 
+        }
+    }
+    
 }
